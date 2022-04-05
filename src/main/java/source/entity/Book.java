e30b5f215ccc2eb4f62b8eca88bb9a90a35eb9e6
MariuszKucinski <kucinskimariusz00@gmail.com>
2022-04-05 03:09:05 +0200

    Validate book

--- src/main/java/source/entity/Book.java
+++ src/main/java/source/entity/Book.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @Getter
@@ -20,10 +23,28 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotEmpty
+    @NotNull
     private String isbnNumber;
+
+    @NotEmpty
+    @NotNull
+    @Size(min = 5)
     private String title;
+
+    @NotEmpty
+    @NotNull
+    @Size(min = 2, max = 50)
     private String author;
+
+    @NotEmpty
+    @NotNull
+    @Size(min=2)
     private String publisher;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 2, max = 100)
     private String type;
 
     public Book(){}
